eb53c887271ea33c7a3e8cb258cdf7d96437552f
Gavin Rehkemper <gavin@gavinr.com>
2021-11-09 00:25:35 -0600

    init

---
+++ src/routes/index.svelte
@@ -0,0 +1,12 @@
+<script>
+	const links = [...Array(500).keys()];
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<ul>
+	{#each links as link}
+		<li><a sveltekit:prefetch href="/links/{link}">{link}</a></li>
+	{/each}
+</ul>
